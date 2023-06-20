122c1e4a00eb89c71d5496faeb49c652062a6d27
Kirigaine <tholen2147@gmail.com>
2023-06-19 23:58:46 -0500

    Completed 1 7kyu and 2 8kyu's

---
+++ 8kyu/Multiply.py
@@ -0,0 +1,10 @@
+# ***DESCRIPTION***
+# This code does not execute properly. Try to figure out why.
+
+# ***BASE CODE***
+# def multiply(a, b):
+    # a * b
+    
+# ***CODE***
+def multiply(a, b):
+    return a * b
---
+++ 8kyu/Reversed Strings.py
@@ -0,0 +1,13 @@
+# ***DESCRIPTION***
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow
+
+# ***BASE CODE***
+# def solution(string):
+    # pass
+    
+# ***CODE***
+def solution(string):
+    return string[::-1]
